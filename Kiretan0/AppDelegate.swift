cd4d88171b709ad3f5b81f254e119b63dd0c1967
Hironori Ichimiya <hiron@hironytic.com>
2017-08-21 22:41:03 +0900

    Onboard Firebase SDK

--- Kiretan0/AppDelegate.swift
+++ Kiretan0/AppDelegate.swift
@@ -24,6 +24,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -32,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.3'
+
+target 'Kiretan0' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Kiretan0
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+
+  target 'Kiretan0Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
